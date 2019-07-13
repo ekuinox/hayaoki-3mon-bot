43c67522763d9fb252c7462117a7b3b36df835dc
lm9 <depkey@me.com>
2019-07-13 14:30:06 +0900

    dotenv

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 const Bot = require('./bot')
 
-const bot = new Bot('', '')
+require('dotenv').config()
+
+const LINE_CHANNEL_ACCESS_TOKEN = process.env.LINE_CHANNEL_ACCESS_TOKEN
+const LINE_CHANNEL_SECRET = process.env.LINE_CHANNEL_SECRET
+
+const bot = new Bot(LINE_CHANNEL_ACCESS_TOKEN, LINE_CHANNEL_SECRET)
 
 bot.run()
